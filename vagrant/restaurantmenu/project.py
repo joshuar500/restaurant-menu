6b3ede27c19f8b42ac47959d355761e5b7505856
Josh Rincon <memonkey@gmail.com>
2015-03-18 22:18:39 -0700

    Added new item functionality

--- vagrant/restaurantmenu/project.py
+++ vagrant/restaurantmenu/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -20,17 +20,29 @@ def restaurant_menu(restaurant_id):
     return render_template('menu.html', restaurant=restaurant, items=items)
 
 
-@app.route('/restaurants/<int:restaurant_id>/new')
+@app.route(
+    '/restaurants/<int:restaurant_id>/new',
+    methods=['GET', 'POST'])
 def new_menu_item(restaurant_id):
-    return "page to create a new menu item"
+    if request.method == 'POST':
+        newItem = MenuItem(name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurant_menu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit')
+@app.route(
+    '/restaurants/<int:restaurant_id>/<int:menu_id>/edit',
+    methods=['GET', 'POST'])
 def edit_menu_item(restaurant_id, menu_id):
     return "page to edit a menu item"
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete')
+@app.route(
+    '/restaurants/<int:restaurant_id>/<int:menu_id>/delete',
+    methods=['GET', 'POST'])
 def delete_menu_item(restaurant_id, menu_id):
     return "page to delete a menu item"
 
